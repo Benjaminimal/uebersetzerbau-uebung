3bde3d7ae25edb7e1c909c738e8903f853ac57a1
ublu2020 00940280 <u00940280@g0.complang.tuwien.ac.at>
2020-03-14 20:34:38 +0100

    UE1: Save constants in data segment

--- asma/asma.s
+++ asma/asma.s
@@ -1,7 +1,14 @@
     .file   "asma.c"
     .text
+    .p2align 4,,15
     .globl  asma
     .type   asma, @function
+.LC0:
+    .skip 16, 0x9a                      # 'z' + 1 + min_t - 'a'
+.LC1:
+    .skip 16, 0x1f                      # min_t - 'a'
+.LC2:
+    .skip 16, 0xe0                      # 'A' - 'a'
 asma:
 .LFB0:
     .cfi_startproc
@@ -9,29 +16,15 @@ asma:
 
     movdqu  (%rdi), %xmm0               # copy first 16 chars of string to vector register
 
-    movq    $0x9a9a9a9a9a9a9a9a, %rcx   # set up ('z' + 1 + min_t - 'a') vector
-    push    %rcx
-    push    %rcx
-    movdqu  (%rsp), %xmm1
-
-    movq    $0x1f1f1f1f1f1f1f1f, %rcx   # set up (c + min_t - 'a') vector
-    push    %rcx
-    push    %rcx
-    movdqu  (%rsp), %xmm2
-
-    movq    $0xe0e0e0e0e0e0e0e0, %rcx   # set up ('A' - 'a') vector
-    push    %rcx
-    push    %rcx
-    movdqu  (%rsp), %xmm3
+    movdqa  .LC0(%rip), %xmm1           # set up ('z' + 1 + min_t - 'a') vector
+    movdqa  .LC1(%rip), %xmm2           # set up (c + min_t - 'a') vector
 
     paddb   %xmm0, %xmm2                # compute (c + min_t - 'a')
     pcmpgtb %xmm2, %xmm1                # generate ('z' + 1 + min_t - 'a') > (c + min_t - 'a') bit mask
-    pand    %xmm3, %xmm1                # sieve ('A' - 'a') with bit mask
+    pand    .LC2(%rip), %xmm1           # sieve ('A' - 'a') with bit mask
     paddb   %xmm1, %xmm0                # perform conditional computation of (c + ('A' - 'a'))
 
     movdqu  %xmm0, (%rax)               # write result
-
-    sub     $-48, %rsp                  # restore stack pointer
 
     rep ret                             # return
     .cfi_endproc
