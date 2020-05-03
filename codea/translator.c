053999b5e32b405e9884f03e1af427cf375a60d7
ublu2020 00940280 <u00940280@g0.complang.tuwien.ac.at>
2020-05-03 21:05:02 +0200

    UE6: Fix broken code generation for not and neg

--- codea/translator.c
+++ codea/translator.c
@@ -106,15 +106,15 @@ void cmp_i(long long val, char *dst) {
 }
 
 void leq(char *dst) {
-    printf("\tsetle\t%%%s, %%%s\n", get_reg(dst)->b_name);
+    printf("\tsetle %%%s\n", get_reg(dst)->b_name);
 }
 
 void gt(char *dst) {
-    printf("\tsetg\t%%%s, %%%s\n", get_reg(dst)->b_name);
+    printf("\tsetg %%%s\n", get_reg(dst)->b_name);
 }
 
 void dif(char *dst) {
-    printf("\tsetne\t%%%s, %%%s\n", get_reg(dst)->b_name);
+    printf("\tsetne %%%s\n", get_reg(dst)->b_name);
 }
 
 void and(char *src, char *dst) {
